84ac02470e9c4b439164854263b1ff6874967ce5
Brad West <brad.west@workiva.com>
2019-02-14 22:04:06 -0700

    Add send message function, c-ss functionality

--- pa2/client_udp.py
+++ pa2/client_udp.py
@@ -4,32 +4,58 @@
 Implements a grenade throwing client
 """
 
+import argparse
 import logging
-import socket
-import sys
+import time
 
 import messages
 
-from server_udp import open_socket, parse_arguments
+from server_udp import open_socket
 
 
-def send_messages(socket, server_address, server_port):
+def send_messages_for_time(client_socket,
+                           server_address,
+                           server_port,
+                           message_len,
+                           duration):
     """
-    Send messages to server
-    :param socket.Socket socket:
+    Send messages tos server
+    :param socket.SSLSocket client_socket:
     :param str server_address:
     :param int server_port:
+    :param int message_len: message length in bytes
+    :param int duration:
     :return:
     """
-    pass
+    message = messages.create_message_of_len(message_len)
+    end_time = time.time() + duration
+    while time.time() < end_time:
+        # TODO Do we need to throttle this to wait for a message?
+        client_socket.sendto(message.encode(), (server_address, server_port))
+        _, _ = client_socket.recvfrom(2048)
 
 
 def main():
     logging.getLogger().setLevel(logging.INFO)
-    args = parse_arguments(sys.argv[1:])
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--ports', default=12000, type=int,
+                        help='port to receive messages on')
+    parser.add_argument('-c', '--server-address',
+                        help='server ip to send to')
+    parser.add_argument('-k', '--server-port', default=12000, type=int,
+                        help="server port to send to")
+    parser.add_argument('-b', '--bytes', help='size of message in bytes',
+                        defaut=1, type=int)
+    parser.add_argument('-r', '--rate', help='rate to send message in msg/sec',
+                        default=1, type=int)
+    parser.add_argument('-d', '--duration', type=int, default=30,
+                        help='time in seconds to keep socket open')
+    args = parser.parse_args()
 
     client_socket = open_socket(args.port)
-    send_messages(client_socket, args.server_address, args.server_port)
+    send_messages_for_time(client_socket, args.server_address, args.server_port,
+                  args.bytes, args.duration)
 
 
 if __name__ == "__main__":
--- pa2/server_udp.py
+++ pa2/server_udp.py
@@ -7,7 +7,7 @@ Implements the server in the Grenade game. Some code adopted from Kurose
 import argparse
 import logging
 import socket
-import sys
+import time
 
 import messages
 
@@ -23,54 +23,77 @@ def open_socket(port):
     logging.info('Socket opened on port {}'.format(port))
 
 
-def receive_and_process_on_socket(server_socket, client_address=None, client_port=None):
+def receive_and_return_on_socket(server_socket, q):
     """
     Receives messages on the socket and routes/processes appropriately
-    :param socket.Socket server_socket:
-    :param str client_address:
-    :param int client_port:
+    :param socket.SSLSocket server_socket:
+    :param int q: question
     :return:
     """
+    # TODO it's going to be easier to send to multiple clients than to have
+    #  multiple clients simultaneously send to the server. Fix that
     while True:
         message, client_address = server_socket.recvfrom(2048)
-        message_decoded = message.decode()
-        logging.INFO('Received: {}'.format(message_decoded))
-        # TODO Implement control for what the client message is
-        # server_socket.sendto(server_message, client_address)
+        if q in {1, 2, 3, 4}:
+            logging.INFO('Q{}, received: {}'.format(q, message.decode()))
+            server_socket.sendto(message, client_address)
 
 
-def receive_and_process(server_ports):
+def receive_and_process(server_ports,
+                        client_addresses_str=None,
+                        client_ports_str=None,
+                        message_len=None,
+                        duration=None):
     """
     Opens sockets (multiple if necessary) and starts communications
     :param str server_ports: comma separated list of server ports
+    :param str client_addresses_str:
+    :param int client_ports_str:
+    :param int message_len: message length in bytes
+    :param int duration: duration in seconds for sending
     """
     server_sockets = [open_socket(int(p)) for p in server_ports.split(",")]
-    for s in server_sockets:
-        receive_and_process_on_socket(s)
-
-
-# TODO could extend the argparse class
-def parse_arguments(args):
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--ports', default="12000",
-                        help='comma separated list of ports to receive messages on')
-    parser.add_argument('-c', '--server-address',
-                        help='server ip to send to')
-    parser.add_argument('-k', '--server-port', default=12000, type=int,
-                        help="server port to send to")
-    parser.add_argument('-b', '--bytes', help='size of message in bytes',
-                        defaut=1, type=int)
-    parser.add_argument('-r', '--rate', help='rate to send message in msg/sec',
-                        default=1, type=int)
-    parser.add_argument('-q', '--question', required=True,
-                        help='the integer numbered question, see readme')
-    return parser.parse_args(args)
+    if client_addresses_str and client_ports_str:
+        client_addresses = client_addresses_str.split(",")
+        client_ports = [int(p) for p in client_ports_str.split(",")]
+        message = messages.create_message_of_len(message_len)
+        end_time = time.time() + duration
+        while time.time() < end_time:
+            for s, c, p in zip(server_sockets, client_addresses, client_ports):
+                # TODO parallelize this?
+                s.sendto(message.encode(), (c, p))
+                _, _ = s.recvfrom(2048)
+    else:
+        for s in server_sockets:
+            receive_and_return_on_socket(s)
 
 
 def main():
     logging.getLogger().setLevel(logging.INFO)
-    args = parse_arguments(sys.argv[1:])
-    receive_and_process(args.ports)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--ports', default="12000",
+                        help='comma separated list of ports to receive messages on')
+    parser.add_argument('-c', '--client-addresses',
+                        help='comma separated list of addresses to send to')
+    parser.add_argument('-k', '--client-ports', default="12000",
+                        help="comma separated list of ports to send to")
+    parser.add_argument('-b', '--bytes', help='size of message in bytes',
+                        defaut=1, type=int)
+    parser.add_argument('-r', '--rate', help='rate to send message in msg/sec',
+                        default=1, type=int)
+    parser.add_argument('-d', '--duration', type=int, default=30,
+                        help='time in seconds to keep socket open')
+    parser.add_argument('-q', '--question', required=True,
+                        help='the integer numbered question, see readme')
+    args = parser.parse_args()
+
+    if (args.client_addresses and not args.client_ports) or \
+            (args.client_ports and not args.client_addresses):
+        parser.error("--client-port and client-addresses must both be specified if one is")
+
+
+    receive_and_process(args.ports, args.question)
 
 
 if __name__ == "__main__":
