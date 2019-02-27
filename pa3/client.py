6c2a1d24888ea2bada562050e71deb1523baeb4c
Brad West <brad.west@workiva.com>
2019-02-26 21:59:49 -0700

    Add parse_args

--- pa3/client.py
+++ pa3/client.py
@@ -1,3 +1,5 @@
 """
 Client functionality
+
+https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sqs.html
 """
--- pa3/messages.py
+++ pa3/messages.py
@@ -1,3 +1,20 @@
 """
 Functionality shared between client and server
 """
+
+import argparse
+
+
+def parse_arguments(sys_args):
+    """
+    Client and server should share the same arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-sq' '--send-queue', help='queue to publish to')
+    parser.add_argument('-cq' '--consume-queue', help='queue to consume')
+    parser.add_argument('-l', '--latency', help='desired latency')
+    parser.add_argument('-b', '--bytes', help='message size (bytes)', default=1, type=int)
+    parser.add_argument('-r', '--rate', help='message rate (msg/sec)', default=1, type=int)
+    parser.add_argument('-n', '--num-messages', default=52, type=int)
+    args = parser.parse_args(sys_args)
+    return args
