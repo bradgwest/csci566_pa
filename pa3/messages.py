37b19ec2448d801c6ddfdf2a8cc09fc371293670
Brad West <brad.west@workiva.com>
2019-03-02 11:48:12 -0700

    create message attributes and rate to sec func

--- pa3/messages.py
+++ pa3/messages.py
@@ -19,17 +19,31 @@ def parse_arguments(sys_args):
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--send-queue', help='queue to publish to')
-    parser.add_argument('-c', '--consume-queue', help='queue to consume')
+    parser.add_argument('-r', '--receive-queue', help='queue to consume')
     parser.add_argument('-d', '--delete-queue', help='queue to push delete messages to')
-    parser.add_argument('-b', '--bytes', help='message size (bytes)', default=1, type=int)
-    parser.add_argument('-t', '--rate', help='message rate (msg/sec)', default=1, type=int)
-    parser.add_argument('-n', '--num-messages', default=52, type=int)
+    parser.add_argument('--size', help='message sizes (bytes). The cross product '
+                                       'of this and rate will be used. Like 1,2,4,8,16,...',
+                        default=1, type=int)
+    parser.add_argument('--rate', help='message rate (msg/sec). The cross product of '
+                                       'this and bytes will be used. Like 1,2,4,8,...',
+                        default=1, type=int)
+    parser.add_argument('--num-messages', default=52, type=int)
     parser.add_argument('--device', help='network device', default='eth0')
     parser.add_argument('--latency', help='desired latency (ms)', default=15, type=int)
     parser.add_argument('--loss', help='packet loss (%)', default=1, type=int)
     parser.add_argument('--bandwidth', help='bandwidth (kbits/sec)', default=10000, type=int)
     parser.add_argument('-f', '--log-file', help='log file to write to', required=True)
+    parser.add_argument('-t', '--comm-type', help='communication type, one of csc, or scc', default='csc')
+    parser.add_argument('--sender', help='if scc is specified, whether this is a sender or not', action='store_true')
+    parser.add_argument('-c', 'num-clients', help='number of clients')
     args = parser.parse_args(sys_args)
+
+    if args.comm_type not in {'csc', 'scc'}:
+        raise ValueError('comm_type must be either csc or scc')
+
+    if args.comm_type == 'scc' and not args.sender:
+        raise ValueError('sender must be specified if comm_type is scc')
+
     return args
 
 
@@ -73,3 +87,27 @@ def setup_log(log_file, name='logger'):
     logger.addHandler(ch)
     logger.addHandler(fh)
     return logger
+
+
+def rate_to_sec(rate=None):
+    """
+    Converts a rate (messages/sec) to a time to delay
+    :param int rate:
+    :rtype: int
+    """
+    if not rate:
+        return 0.0
+    return 1/rate
+
+
+def create_message_attributes(size, rate):
+    return {
+        'size': {
+            'DataType': 'Number',
+            'StringValue': str(size)
+        },
+        'rate': {
+            'DataType': 'Number',
+            'StringValue': str(rate)
+        }
+    }
