f2023f4a6e3618366b60232b8cf907a0c44be80d
Brad West <brad.west@workiva.com>
2019-03-02 07:57:43 -0700

    Rename client to handler

--- pa3/handler.py
+++ pa3/handler.py
@@ -4,6 +4,8 @@ Client functionality
 https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sqs.html
 """
 
+import logging
+import string
 import sys
 import threading
 
@@ -12,7 +14,7 @@ import boto3
 import messages
 
 
-class ClientMessageThread(threading.Thread):
+class MessageHandlerThread(threading.Thread):
 
     def __init__(self, client):
         self.client = client
@@ -21,26 +23,34 @@ class ClientMessageThread(threading.Thread):
         handler(self.client, *args)
 
 
-def send_messages(client, sent_queue):
+def send_messages(client, send_queue, num_messages, bytes, rate):
     pass
 
 
 def receive_messages(client, receive_queue, delete_queue):
+    if not receive_queue:
+        logging.warning("No receive_queue specified, exiting")
+        return
     pass
 
 
 def main():
     args = messages.parse_arguments(sys.argv[1:])
     _ = messages.setup_log(args.log_file, 'client_logger')
-    # Setup client
+    # setup networking
+    messages.set_tc(args.device, args.latency, args.loss, args.bandwidth)
+    # setup client
     client = boto3.client('sqs')
-    # Create and start threads
-    sender = ClientMessageThread(client=client)
-    receiver = ClientMessageThread(client=client)
+    # create, start, and run threads
+    sender = MessageHandlerThread(client=client)
+    receiver = MessageHandlerThread(client=client)
     sender.start()
     receiver.start()
-    sender.run(send_messages, args.send_queue)
+    sender.run(send_messages, args.send_queue, args.num_messages, args.bytes,
+               args.rate)
     receiver.run(receive_messages, args.consume_queue, args.delete_queue)
+    # delete networking rules
+    messages.delete_tc(args.device)
 
 
 if __name__ == "__main__":
