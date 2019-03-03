6e41af0a4ef74094d187c7bd3d5e818bf3ffd789
Brad West <brad.west@workiva.com>
2019-03-03 10:23:16 -0700

    Add question3 to run experiment

--- pa3/run_experiment.py
+++ pa3/run_experiment.py
@@ -3,8 +3,11 @@ Remotely invokes an experiment
 """
 
 import argparse
+import logging
 import os
 import subprocess
+import sys
+import threading
 
 import boto3
 
@@ -30,36 +33,61 @@ def setup_queues(sqs_client):
 
 
 def copy_logs(instance, local_directory, remote_file):
+    remote_loc = "ubuntu@{}:{}".format(instance, remote_file)
+    logging.info("copying logs from {} to {}".format(remote_loc, remote_file))
     if not os.path.exists(local_directory):
         os.mkdir(local_directory)
-    args = ["scp", "ubuntu@{}:{}".format(instance, remote_file), local_directory]
+    args = ["scp", remote_loc, local_directory]
     subprocess.run(args)
 
 
-def setup_and_run_experiment(question, ec2_client_dns, ec2_server_dns):
+# def run_handler_subprocess(args, shell=True, capture_output=True):
+#     # completed_process = subprocess.run(args, shell=shell,
+#     #                                    capture_output=capture_output)
+#     # completed_process.check_returncode()
+#     ssh = subprocess.Popen(args, shell=shell, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#     result = ssh.stdout.readlines()
+#     if not result:
+#         error = ssh.stderr.readlines()
+#         print(sys.stderr, "ERROR: %s" % error)
+#     else:
+#         print(result)
+#     return
+
+
+def setup_and_run_experiment(question, ec2_client_dns, ec2_server_dns=None):
     sqs = boto3.client("sqs")
     sqs_queues = setup_queues(sqs)
+    logging.info("Created queues: {}", sqs_queues)
     if question == 3:
-        logfile = "q3.log"
-        args_server = ["python3", "handler.py",
-                       "--send-queue", sqs_queues[1],
-                       "--receive-queue", sqs_queues[0],
-                       "--log-file", logfile,
-                       "--comm-type", "csc",
-                       "--server"]
-        completed_process = subprocess.run(args_server, shell=True)
-        completed_process.check_returncode()
-        args_client = ["python3", "handler.py",
-                       "--send-queue", sqs_queues[0],
-                       "--receive-queue", sqs_queues[1],
-                       "--size", "1,2,4,8,16,32,64,128,...",
-                       "--rate", 0,
-                       "--log-file", logfile,
-                       "--comm-type", "csc"]
-        completed_process = subprocess.run(args_client, shell=True)
-        completed_process.check_returncode()
+        logging.info("Running question 3")
+        logfile = "log/q3.log"
+        py_server_cmd = "python3 handler.py --send-queue {} " \
+                        "--receive-queue {} --log-file {} --comm-type csc " \
+                        "--server".format(sqs_queues[0], sqs_queues[1], logfile)
+        py_client_cmd = "python3 handler.py --send-queue {} " \
+                        "--receive-queue {} --size 1 --rate 0 --log-file {} " \
+                        "--comm-type csc".format(sqs_queues[0], sqs_queues[1], logfile)
+        print("Run on server:")
+        print(py_server_cmd)
+        print("Continue? [y/n]")
+        a = input()
+        if a != "y":
+            print("Quitting")
+            return
+        print("Run on client:")
+        print(py_client_cmd)
+        print("Continue? [y/n]")
+        if a != "y":
+            print("Quitting")
+            return
+        print("Continue to copy logs when processes have completed")
+        a = input("Copy Logs? [y/n]")
+        if a != "y":
+            print("Quitting")
+            return
         copy_logs(ec2_client_dns, local_directory="log/q3/",
-                  remote_file="~/log/{}".format(logfile))
+                  remote_file="~/{}".format(logfile))
     elif question == 4:
         pass
     else:
@@ -72,13 +100,13 @@ def setup_and_run_experiment(question, ec2_client_dns, ec2_server_dns):
 
 
 def main():
+    logging.getLogger().setLevel(logging.INFO)
     parser = argparse.ArgumentParser()
-    parser.add_argument('--question', help='question number', type=int, required=True)
-    parser.add_argument('--ec2-client-dns', help='public dns of ec2 client instance', required=True)
-    parser.add_argument('--ec2-server-dns', help='public dns of ec2 server instance', required=True)
+    parser.add_argument('question', help='question number', type=int)
+    parser.add_argument('-c', '--client', help='public dns of ec2 client instance')
+    parser.add_argument('-s', '--server', help='public dns of ec2 server instance')
     args = parser.parse_args()
-    setup_and_run_experiment(args.question, args.ec2_client_dns,
-                             args.ec2_server_dns)
+    setup_and_run_experiment(args.question, args.client, args.server)
 
 
 if __name__ == '__main__':
