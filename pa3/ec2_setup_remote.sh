9a4ea346fa112f911e921a3a67cf41eef9735fc2
Brad West <brad.west@workiva.com>
2019-03-02 19:30:49 -0700

    credentials

--- pa3/ec2_setup_remote.sh
+++ pa3/ec2_setup_remote.sh
@@ -9,6 +9,11 @@ pip3 --version
 sudo apt -y install iproute2
 sudo apt -y install iperf
 
+mkdir ~/.aws
+echo [default] > ~/.aws/config
+echo output = text >> ~/.aws/config
+echo region = us-east-1 >> ~/.aws/config
+
 # install dependencies
 mkdir log
 pip3 install -Ur requirements.txt
