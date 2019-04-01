c37fcb7e92719e2fabe748364d501e357b249bce
rocky1991 <hongchuan91@gmail.com>
2019-04-01 06:14:49 -0600

    fixed some bugs

--- pa4/net_setting.sh
+++ pa4/net_setting.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+sudo tc qdisc del dev eth0 root
 sudo tc qdisc add dev eth0 root handle 1: netem delay 15ms loss 1%
 sudo tc qdisc add dev eth0 parent 1:1 handle 10: tbf rate 10000kbit latency 15ms burst 32kbit
